8cbc12fe84efd4de3df02832f944f1e89dcb68c3
dtho100 <daleythomsen@gmail.com>
2025-11-14 12:44:38 -0500

    time functionality

--- src/notebooks/post_session_data_input.py
+++ src/notebooks/post_session_data_input.py
@@ -2,22 +2,26 @@ import pandas as pd
 import argparse
 import datetime
 
-questions = [
-    {"time_of_day": "morning","questions": {
+user_input = {
+    "morning": {
+     "questions": [
         {"name": "date", "prompt": "What date are you entering data for (YYYY-MM-DD)? ", "type": "date"},
         {"name": "body_weight", "prompt": "Enter morning BW: ", "type": "float"},
         {"name": "sleep", "prompt": "Enter number of hours slept: ", "type": "float"},
         {"name": "rhr", "prompt": "Enter RHR from last night: ", "type": "float"},
-        {"name": "hrv", "prompt": "Enter HRV from last night: ", "type": "float"}}
-        },
-    {"time_of_day": "evening","questions": {
-
+        {"name": "hrv", "prompt": "Enter HRV from last night: ", "type": "float"}
+     ]
+     },
+    "evening": {
+     "questions": [
+        {"name": "date", "prompt": "What date are you entering data for (YYYY-MM-DD)? ", "type": "date"},
         {"name": "pain_notes", "prompt": "Enter any pains (out of 10) and where they occur: ", "type": "str"},
         {"name": "session_1_notes", "prompt": "Enter any Session 1 notes: ", "type": "str"},
         {"name": "session_2_notes", "prompt": "Enter any Session 2 notes: ", "type": "str"},
-        {"name": "general_notes", "prompt": "Enter any other notes: ", "type": "str"}}
+        {"name": "general_notes", "prompt": "Enter any other notes: ", "type": "str"}
+     ]
     }
-    ]
+}
 
 csv_schema_mapping = {
     "date": "Date",
@@ -33,13 +37,44 @@ csv_schema_mapping = {
 
 def read_csv(path=None, file_name='post_session_data.csv'):
     if path is None:
-        df = pd.read_csv(f"/Users/daleythomsen/Documents/GitHub/TriPT/data/raw/post_workout/{file_name}")
+        df = pd.read_csv(f"/Users/daleythomsen/Documents/GitHub/TriPT/data/raw/post_workout/{file_name}",         
+            dtype={
+                "Body Weight": "float64",
+                "Sleep (h)": "float64",
+                "Resting HR": "float64",
+                "HRV (night)": "float64",
+                "Pain Notes": "string",
+                "Session 1 Personal Notes": "string",
+                "Session 2 Personal Notes": "string",
+                "General Notes": "string",
+                },
+    parse_dates=["Date"])
     else:
-        df = pd.read_csv(path)
-    df = df.dropna(how="all").reset_index(drop=True)  
+        df = pd.read_csv(path,         
+            dtype={
+                "Body Weight": "float64",
+                "Sleep (h)": "float64",
+                "Resting HR": "float64",
+                "HRV (night)": "float64",
+                "Pain Notes": "string",
+                "Session 1 Personal Notes": "string",
+                "Session 2 Personal Notes": "string",
+                "General Notes": "string",
+                },
+    parse_dates=["Date"])
+    df = df.dropna(how="all").reset_index(drop=True)
+    # df["Date"] = pd.to_datetime(df["Date"])
+    # df["Body Weight"] = pd.to_float(df["Body Weight"])
+    # df["Pain Notes"] = pd.to_string(df["Date"])
+    # df["Sleep (h)"] = pd.to_float(df["Date"])
+    # df["Resting HR"] = pd.to_float(df["Date"])
+    # df["HRV (night)"] = pd.to_float(df["Date"])
+    # df["Session 1 Personal Notes"] = pd.to_string(df["Date"])
+    # df["Session 2 Personal Notes"] = pd.to_string(df["Date"])
+    # df["General Notes"] = pd.to_string(df["Date"])
     return df
 
-def gather_input(questions=questions):
+def gather_input(questions=user_input):
     for question in questions:
         while True:
             user_answer = input(question["prompt"]).strip()
@@ -74,6 +109,7 @@ def gather_input(questions=questions):
 
 def write_data(user_answers, df, csv_schema_mapping=csv_schema_mapping, path=None, file_name='post_session_data.csv'):
     mapped_user_answers = pd.DataFrame([dict((csv_schema_mapping[key], value) for (key, value) in user_answers.items())])
+    mapped_user_answers.set_index("Date")
     if df.empty:
         df = mapped_user_answers
     else:
@@ -81,8 +117,12 @@ def write_data(user_answers, df, csv_schema_mapping=csv_schema_mapping, path=Non
             while True:
                 user_input = input(f"Data for {mapped_user_answers['Date'].iloc[0]} found. Do you want to overwrite? (yes, no): ")
                 if (user_input.lower() == 'yes') | (user_input.lower() == 'y'):
-                    df = df[~df["Date"].eq(mapped_user_answers["Date"].iloc[0])]
-                    df = pd.concat([df, mapped_user_answers], ignore_index=True)
+                    df.set_index("Date")
+                    df.update(mapped_user_answers)
+                    # df = pd.concat([df[~df.index.isin(mapped_user_answers.index)], mapped_user_answers])
+                    # df.update(mapped_user_answers)
+#                    df = df[~df["Date"].eq(mapped_user_answers["Date"].iloc[0])]
+ #                   df = pd.concat([df, mapped_user_answers], ignore_index=True)
                     break
                 elif (user_input.lower() == 'no') | (user_input.lower() == 'n'):
                     break
@@ -90,6 +130,7 @@ def write_data(user_answers, df, csv_schema_mapping=csv_schema_mapping, path=Non
                     print('Type yes or no')
         else:
             df = pd.concat([df, mapped_user_answers], ignore_index=True)
+#    print(df)
     if path is None:
         df.to_csv(f"/Users/daleythomsen/Documents/GitHub/TriPT/data/raw/post_workout/{file_name}", index=False)
         print(f"File written to /Users/daleythomsen/Documents/GitHub/TriPT/data/raw/post_workout/{file_name}")
@@ -102,10 +143,17 @@ def write_data(user_answers, df, csv_schema_mapping=csv_schema_mapping, path=Non
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Update post-session training log.")
-    parser.add_argument("--path", default=None)
-    parser.add_argument("--file", default='post_session_data.csv')
+    parser.add_argument("-p", "--path", default=None)
+    parser.add_argument("-f", "--file", default='post_session_data.csv')
+    parser.add_argument("-t", "--time", default='both')
     args = parser.parse_args()
 
     df = read_csv(path=args.path, file_name=args.file)
-    user_answers = gather_input(questions)
+
+    if args.time == "both":
+        user_input = user_input["morning"]["questions"] + user_input["evening"]["questions"]
+        user_input = list({question["name"]:question for question in user_input}.values())
+    else:
+        user_input = user_input[args.time]["questions"]
+    user_answers = gather_input(user_input)
     write_data(user_answers=user_answers, df=df, csv_schema_mapping=csv_schema_mapping, file_name=args.file, path=args.path)
